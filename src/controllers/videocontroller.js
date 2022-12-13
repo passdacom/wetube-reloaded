2c861e2b13c9fdf08799a3e59e325d62da7fa8b1
passdacom <42435202+passdacom@users.noreply.github.com>
2022-12-14 05:29:06 +0900

    .

--- src/controllers/videocontroller.js
+++ src/controllers/videocontroller.js
@@ -1,5 +1,46 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
-export const see= (req, res) => res.render("watch", {pageTitle:  "Watch"});
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "3 minutes ago",
+        views: 59,
+        id: 1
+
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "3 minutes ago",
+        views: 59,
+        id: 2
+
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "3 minutes ago",
+        views: 59,
+        id: 3
+
+    },
+];
+
+export const trending = (req, res) => {
+    return res.render("home", {pageTitle: "Home", videos});
+};
+
+
+export const watch= (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+
+    res.render("watch", {pageTitle:  `Watching ${video.title}`, video });
+};
+
+
 export const edit = (req, res) =>  res.render("edit", {pageTitle: "Edit"});
 
 export const search = (req, res) => res.send("Search the Video");
@@ -9,4 +50,5 @@ export const upload = (req, res) => res.send("Upload the Video");
 export function deleteVideo(req, res) {
     
     return res.send("Delete the Video");
+
 }
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-import { edit, see, upload, deleteVideo } from "../controllers/videocontroller";
+import { edit, watch, upload, deleteVideo } from "../controllers/videocontroller";
 
 const videoRouter = express.Router();
 
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
